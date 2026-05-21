b132717f4ebd326b030c8c8a5a1c2570ab12dd5c
Sanjay P <ponnusamyponni2003@gmail.com>
2026-05-21 11:19:55 +0530

    Task-3

--- Task-3/Script.js
+++ Task-3/Script.js
@@ -4,16 +4,16 @@
 // → print "Login Success"
 // Else print "Invalid User"
 
-// let username=prompt("Enter the username?")
-// let password=prompt("Enter the password")
-// if(username=="admin" && password=="1234")
-// {
-//     console.log("Login Success");
-// }
-// else
-// {
-//     console.log("Invalid User");
-// }
+let username=prompt("Enter the username?")
+let password=prompt("Enter the password")
+if(username=="admin" && password=="1234")
+{
+    console.log("Login Success");
+}
+else
+{
+    console.log("Invalid User");
+}
 
 
 // 2. ATM Balance System
@@ -22,16 +22,16 @@
 // If amount less than balance → "Withdraw Success"
 // Else → "Insufficient Balance"
 
-// let balance=5000
-// let amount=prompt("Enter the Withdraw Amount")
-// if(amount<=balance)
-// {
-//     console.log("Withdraw Success");
-// }
-// else
-// {
-//     console.log("Insufficient Balance"); 
-// }
+let balance=5000
+let amount=prompt("Enter the Withdraw Amount")
+if(amount<=balance)
+{
+    console.log("Withdraw Success");
+}
+else
+{
+    console.log("Insufficient Balance"); 
+}
 
 
 // 3. Traffic Signal Project
@@ -40,21 +40,18 @@
 // "yellow" → ready
 // "green" → go
 
-// let color="green"
-// switch(color)
-// {
-//     case "red":console.log("Stop");
-//     break;
-
-//     case "yellow":console.log("Ready");
-//     break;
-
-//     case "green":console.log("Go");
-//     break;
-
-//     default:console.log("Please wait for signal");
-//     break;
-// }
+let color="green"
+switch(color)
+{
+    case "red":console.log("Stop");
+    break;
+    case "yellow":console.log("Ready");
+    break;
+    case "green":console.log("Go");
+    break;
+    default:console.log("Please wait for signal");
+    break;
+}
 
 
 // 4. Age Eligibility Checker
@@ -62,15 +59,15 @@
 // age >= 18 → "Eligible for Vote"
 // Else → "Not Eligible"
 
-// let age=prompt("Enter your Age?")
-// if(age>=18)
-// {
-//     console.log("Eligible for Vote"); 
-// }
-// else
-// {
-//     console.log("Not Eligible");   
-// }
+let age=prompt("Enter your Age?")
+if(age>=18)
+{
+    console.log("Eligible for Vote"); 
+}
+else
+{
+    console.log("Not Eligible");   
+}
 
 
 // 5. Shopping Discount System
@@ -79,34 +76,34 @@
 // Above 3000 → 10% discount
 // Else → no discount
 
-// let price=7000
-// if(price>5000)
-// {
-//     console.log("20% discount = "+ price*0.2);
-// }
-// else if(price>3000)
-// {
-//     console.log("10% discount = "+ price*0.1);
-// }
-// else
-// {
-//     console.log("No Discount");
-// }
+let price=7000
+if(price>5000)
+{
+    console.log("20% discount = "+ price*0.2);
+}
+else if(price>3000)
+{
+    console.log("10% discount = "+ price*0.1);
+}
+else
+{
+    console.log("No Discount");
+}
 
 // 6. Password Strength Checker
 // Check password length.
 // length >= 8 → "Strong Password"
 // Else → "Weak Password"
 
-// let password="abcd12345"
-// if(password.length >=8)
-// {
-//     console.log("Strong Password");
-// }
-// else
-// {
-//     console.log("Weak Password");
-// }
+let password="abcd12345"
+if(password.length >=8)
+{
+    console.log("Strong Password");
+}
+else
+{
+    console.log("Weak Password");
+}
 
 // 7. Mobile Battery Warning
 // Take battery percentage.
@@ -114,19 +111,19 @@
 // <= 50 → "Battery Medium"
 // Else → "Battery Full"
 
-// let percentage=85;
-// if(percentage <=10)
-// {
-//     console.log("Battery Low"); 
-// }
-// else if(percentage<=50)
-// {
-//     console.log("Battery Medium");  
-// }
-// else
-// {
-//     console.log("Battery Full");  
-// }
+let percentage=85;
+if(percentage <=10)
+{
+    console.log("Battery Low"); 
+}
+else if(percentage<=50)
+{
+    console.log("Battery Medium");  
+}
+else
+{
+    console.log("Battery Full");  
+}
 
 // 8. Employee Salary Bonus
 // Create salary variable.
@@ -134,34 +131,34 @@
 // salary >= 30000 → 10% bonus
 // Else → 5% bonus
 
-// let salary=15000
-// if(salary>=50000)
-// {
-//     console.log("15% bonus");
-// }
-// else if(salary>=30000)
-// {
-//     console.log("10% bonus");
-// }
-// else
-// {
-//     console.log("5% bonus");
-// }
+let salary=15000
+if(salary>=50000)
+{
+    console.log("15% bonus");
+}
+else if(salary>=30000)
+{
+    console.log("10% bonus");
+}
+else
+{
+    console.log("5% bonus");
+}
 
 // 9. Online Food Order
 // Take order amount.
 // Above 499 → "Free Delivery"
 // Else → "Delivery Charge Added"
 
-// let orderAmt=600
-// if(orderAmt>499)
-// {
-//     console.log("Free Delivery");
-// }
-// else
-// {
-//     console.log("Delivery Charge Added");
-// }
+let orderAmt=600
+if(orderAmt>499)
+{
+    console.log("Free Delivery");
+}
+else
+{
+    console.log("Delivery Charge Added");
+}
 
 
 // 10. Movie Ticket Booking
@@ -170,19 +167,19 @@
 // Below 18 → Child ticket
 // Else → Adult ticket
 
-// let age1=21;
-// if(age1<5)
-// {
-//     console.log("Free ticket");
-// }
-// else if(age1<18)
-// {
-//     console.log("Child ticket");
-// }
-// else
-// {
-//     console.log("Adult ticket");
-// }
+let age1=21;
+if(age1<5)
+{
+    console.log("Free ticket");
+}
+else if(age1<18)
+{
+    console.log("Child ticket");
+}
+else
+{
+    console.log("Adult ticket");
+}
 
 // 11. Instagram Likes Counter
 // Using for loop
@@ -193,10 +190,10 @@
 // Like 4
 // Like 5
 
-// for(let i=1;i<=5;i++)
-// {
-//     console.log("Like "+i);  
-// }
+for(let i=1;i<=5;i++)
+{
+    console.log("Like "+i);  
+}
 
 // 12. Countdown Timer
 // Using while loop
@@ -208,13 +205,13 @@
 // 1
 // Start
 
-// let n=5;
-// while(n>=1)
-// {
-//     console.log(n);
-//     n--
-// }
-// console.log("Start");
+let n=5;
+while(n>=1)
+{
+    console.log(n);
+    n--
+}
+console.log("Start");
 
 // 13. OTP Verification
 // Create OTP = 1234
@@ -222,29 +219,29 @@
 // Correct → "OTP Verified"
 // Wrong → "Invalid OTP"
 
-// let otp=1234
-// let userOTP=prompt("Enter your OTP?")
-// if(otp==userOTP)
-// {
-//     console.log("OTP Verified"); 
-// }
-// else
-// {
-//     console.log("Invalid OTP"); 
-// }
+let otp=1234
+let userOTP=prompt("Enter your OTP?")
+if(otp==userOTP)
+{
+    console.log("OTP Verified"); 
+}
+else
+{
+    console.log("Invalid OTP"); 
+}
 
 // 14. E-commerce Cart Total
 // Array:
 // let cart = [200,500,1000,300]
 // Find total cart value using loop.
 
-// let cart = [200,500,1000,300]
-// let sum=0;
-// for(let i=0;i<=cart.length-1;i++)
-// {
-//     sum=sum+cart[i]
-// }
-// console.log(sum);
+let cart = [200,500,1000,300]
+let sum=0;
+for(let i=0;i<=cart.length-1;i++)
+{
+    sum=sum+cart[i]
+}
+console.log(sum);
 
 
 // 15. Student Mark Grade System
@@ -254,23 +251,23 @@
 // 50+ → C Grade
 // Else → Fail
 
-// let mark=90
-// if(mark>=90)
-// {
-//     console.log("A Grade"); 
-// }
-// else if(mark>=70)
-// {
-//     console.log("B Grade");
-// }
-// else if(mark>=50)
-// {
-//     console.log("C Grade");
-// }
-// else
-// {
-//     console.log("Fail");
-// }
+let mark=90
+if(mark>=90)
+{
+    console.log("A Grade"); 
+}
+else if(mark>=70)
+{
+    console.log("B Grade");
+}
+else if(mark>=50)
+{
+    console.log("C Grade");
+}
+else
+{
+    console.log("Fail");
+}
 
 // 16. Netflix Subscription Checker
 // Boolean variable:
@@ -278,30 +275,30 @@
 // true → "Watch Movie"
 // false → "Buy Subscription"
 
-// let subscription=true
-// if(subscription==true)
-// {
-//     console.log("Watch Movie");
-// }
-// else
-// {
-//     console.log("Buy Subscription");
-// }
+let subscription=true
+if(subscription==true)
+{
+    console.log("Watch Movie");
+}
+else
+{
+    console.log("Buy Subscription");
+}
 
 // 17. Product Stock Checker
 // let stock = 0
 // stock > 0 → "Product Available"
 // Else → "Out of Stock"
 
-// let stock=0
-// if(stock>0)
-// {
-//     console.log("Product Available"); 
-// }
-// else
-// {
-//     console.log("Out of Stock");  
-// }
+let stock=0
+if(stock>0)
+{
+    console.log("Product Available"); 
+}
+else
+{
+    console.log("Out of Stock");  
+}
 
 // 18. Weather App Condition
 // Using switch
@@ -309,18 +306,18 @@
 // "rainy" → "Take Umbrella"
 // "cold" → "Wear Jacket"
 
-// let weather="sunny"
-// switch(weather)
-// {
-//     case "sunny":console.log("Go Outside");
-//     break;
-//     case "rainy":console.log("Take Umbrella");
-//     break;
-//     case "cold":console.log("Wear Jacket");
-//     break;
-//     default:console.log("Tell me a weather condition");
-//     break;
-// }
+let weather="sunny"
+switch(weather)
+{
+    case "sunny":console.log("Go Outside");
+    break;
+    case "rainy":console.log("Take Umbrella");
+    break;
+    case "cold":console.log("Wear Jacket");
+    break;
+    default:console.log("Tell me a weather condition");
+    break;
+}
 
 // 19. User Profile Object
 // Create object:
@@ -331,15 +328,15 @@
 // }
 // Print all values using for in loop.
 
-// let user = {
-//    name : "Navi",
-//    age : 20,
-//    city : "Bangalore"
-// }
-// for(let c in user)
-// {
-//     console.log(c,user[c]);
-// }
+let user = {
+   name : "Navi",
+   age : 20,
+   city : "Bangalore"
+}
+for(let c in user)
+{
+    console.log(c,user[c]);
+}
 
 // 20. WhatsApp Chat Array
 // Array:
